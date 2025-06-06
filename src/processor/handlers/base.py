edbd42dbedc61765d2cbf256dddc21fffefd78d3
JORGE HERNAN CARDENAS <jorgehernancardenas@JORGEs-MacBook-Pro.local>
2025-06-06 12:02:07 -0400

    deployed

--- src/processor/handlers/base.py
+++
@@ -1,6 +0,0 @@
-class BaseHandler:
-    def __init__(self, store):
-        self.store = store
-
-    def handle(self, data):
-        raise NotImplementedError("Handle method must be implemented by subclass.")
